2d34a02c14e75b6a4b1940eb0c98db673e6a675c
daguo <1944182256@qq.com>
2023-10-12 20:24:35 +0800

    实现新增文章

--- my_blog/apps/article/serializers.py
+++ my_blog/apps/article/serializers.py
@@ -5,9 +5,10 @@ from apps.article.models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     # id = serializers.IntegerField(read_only=True)
     # title = serializers.CharField(allow_blank=True, max_length=100)
-    # content = serializers.CharField(allow_blank=True)
+    content = serializers.CharField(required=False)
     # create_time = serializers.DateTimeField()
     # update_time = serializers.DateTimeField()
+
     class Meta:
         model = Article
         fields = '__all__'
--- my_blog/apps/article/views.py
+++ my_blog/apps/article/views.py
@@ -3,6 +3,7 @@ from apps.article.models import Article
 from apps.article.serializers import ArticleSerializer
 from rest_framework.views import APIView
 from django.http import JsonResponse
+from rest_framework import status
 from rest_framework.response import Response
 
 
@@ -11,7 +12,13 @@ class ArticleAPIView(APIView):
     def get(self, request):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
